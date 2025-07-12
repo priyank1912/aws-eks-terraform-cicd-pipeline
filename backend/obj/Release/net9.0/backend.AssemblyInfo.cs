2de9f3f1e4ba86c23ce527722fd4a468558c05e7
priyank1912 <priyankshah2345@gmail.com>
2025-07-12 23:22:16 +0530

    added variables in .env file

--- backend/obj/Release/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Release/net9.0/backend.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b782ab5024aeb07cbeac545c60cb449b760adbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
