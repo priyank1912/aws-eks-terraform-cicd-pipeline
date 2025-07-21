beddae501fec005ce9e8f1dd5f54b865ffd45d2c
pixelpix13 <pixelpix99@gmail.com>
2025-07-21 16:10:54 -0500

    Update axios post URL in saveFormData method to use full URL

--- frontend/src/services/databaseService.ts
+++ frontend/src/services/databaseService.ts
@@ -6,7 +6,7 @@ import axios from "axios";
 interface FormData {
   [key: string]: string;
 }
-const API_BASE_URL = "https://backend-service/api";
+const API_BASE_URL = "https://172.20.174.201/api";
 
 export const saveUser = async (user: { username: string; email: string }) => {
   console.log("Sending payload:", user);
