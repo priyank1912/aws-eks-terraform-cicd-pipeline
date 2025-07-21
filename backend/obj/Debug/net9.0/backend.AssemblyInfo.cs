48088d0c2293881ae8346f670082702f3ee76816
pixelpix13 <pixelpix99@gmail.com>
2025-07-21 16:14:41 -0500

    Update API_BASE_URL in databaseService.ts to use HTTP and update AssemblyInfo version

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fb60c68eb0c492283c04134c18a460b34dae0be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beddae501fec005ce9e8f1dd5f54b865ffd45d2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/services/databaseService.ts
+++ frontend/src/services/databaseService.ts
@@ -6,7 +6,7 @@ import axios from "axios";
 interface FormData {
   [key: string]: string;
 }
-const API_BASE_URL = "https://172.20.174.201/api";
+const API_BASE_URL = "http://172.20.174.201/api";
 
 export const saveUser = async (user: { username: string; email: string }) => {
   console.log("Sending payload:", user);
